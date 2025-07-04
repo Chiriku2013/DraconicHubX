87bc5b44fe119b7989197dbcb00dddb95b70fa75
Chiriku2013 <suzukichiriku@gmail.com>
2025-07-04 11:30:55 +0700

    Update DraconicHubX.lua

--- DraconicHubX.lua
+++ DraconicHubX.lua
@@ -10,9 +10,19 @@ if worldMap[placeId] then
         Evade = true
     end
 
--- Load Draconic Hub X
+-- Set UI title/logo/credit - UiLoading
+    getgenv().TitleScript = "Loading Script Kaitun..."
+    getgenv().LogoScript = "rbxassetid://119836305527028"
+    getgenv().CreditScript = "By: Chiriku Roblox"
+
+-- Load UiLoading
 task.spawn(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiriku2013/UiLoading/refs/heads/main/UiLoading.lua"))()
+    end)
+
+-- Load Draconic Hub X
+task.spawn(function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/kasra50/Idk/refs/heads/main/I%20mean%20nobody%20can%20stop%20me.txt'))()
     end)
 else
     game.Players.LocalPlayer:Kick("This script is not supported in this game, please try again with Evade!")
